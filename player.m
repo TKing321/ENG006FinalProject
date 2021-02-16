711054e8a99eaa264ec283b87b9019f3660378c6
TKing <timothyblanton123@gmail.com>
2021-02-16 12:11:18 -0800

    Implemented Player Total
    Made the code to get the total player score.

--- player.m
+++ player.m
@@ -15,6 +15,26 @@ classdef player
             thingSpeakWrite(main.channelIDStatus,temp,'Writekey',main.writeKeyStatus);
         end
         
+        function total = getPlayerScore(obj)
+            temp=obj.board.total("red");
+            for(i=1:1:temp)
+                total=total+i;
+            end
+            temp=obj.board.total("yellow");
+            for(i=1:1:temp)
+                total=total+i;
+            end
+            temp=obj.board.total("blue");
+            for(i=1:1:temp)
+                total=total+i;
+            end
+            temp=obj.board.total("green");
+            for(i=1:1:temp)
+                total=total+i;
+            end
+            total=total-(5*obj.board.playerPenalties);
+        end
+        
     end
     
 end
